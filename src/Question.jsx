de63ca3db5a0c29376881aa6a84fe076c11fda54
jessica4929 <jessica4929@gmail.com>
2024-04-05 12:32:18 -0500

    minus icon

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaPlus } from "react-icons/fa6";
+import { FaPlus, FaMinus } from "react-icons/fa6";
 const Question = ({ id, title, info }) => {
   const [isVisible, setIsVisible] = useState(false);
   return (
@@ -11,7 +11,7 @@ const Question = ({ id, title, info }) => {
           className="btn question-btn"
           onClick={() => setIsVisible(!isVisible)}
         >
-          <FaPlus />
+          {isVisible ? <FaMinus /> : <FaPlus />}
         </button>
       </header>
       {isVisible && <p>{info}</p>}
